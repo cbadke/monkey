850f1c6e757f9ae6544a942bd076b49030f20185
Curtis Badke <curtis.badke@devfacto.com>
2017-08-27 15:54:04 -0600

    Add basic let parsing and error reporting

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -6,26 +6,27 @@ type Lexer struct {
   input        string
   peekPosition int
   lineCount    int
+  charCount    int
   filename     string
 }
 
 func NewLexer(input string, filename string) *Lexer {
-  l := &Lexer{input: input, filename: filename, lineCount: 1}
+  l := &Lexer{input: input, filename: filename, lineCount: 1, charCount: 0}
   return l
 }
 
 func (l *Lexer) readChar() (byte, int, int) {
   var ch byte
-  position := l.peekPosition
 
   if l.peekPosition >= len(l.input) {
     ch = 0
   } else {
     ch = l.input[l.peekPosition]
     l.peekPosition += 1
+    l.charCount += 1
   }
 
-  return ch, position, l.lineCount
+  return ch, l.charCount-1, l.lineCount
 }
 
 func (l *Lexer) peekChar() byte {
@@ -49,92 +50,118 @@ func (l *Lexer) NextToken() token.Token {
     if l.peekChar() == '=' {
       secondChar, _, _ := l.readChar()
       literal := string(firstChar) + string(secondChar)
-      return token.Token{Type: token.EQ, Literal: literal, Filename: l.filename, Line: line, Character: position}
+      return newComplexToken(token.EQ, literal, position, line, l.filename)
     } else {
-      return newToken(token.ASSIGN, firstChar, position, line)
+      return newToken(token.ASSIGN, firstChar, position, line, l.filename)
     }
   case '+':
-    char, line, position := readChar()
-    return newToken(token.PLUS, char, p)
+    char, position, line := l.readChar()
+    return newToken(token.PLUS, char, position, line, l.filename)
   case '(':
-    return newToken(token.LPAREN, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.LPAREN, char, position, line, l.filename)
   case ')':
-    return newToken(token.RPAREN, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.RPAREN, char, position, line, l.filename)
   case '{':
-    return newToken(token.LBRACE, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.LBRACE, char, position, line, l.filename)
   case '}':
-    return newToken(token.RBRACE, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.RBRACE, char, position, line, l.filename)
   case '\'':
-    return newToken(token.SINGLE_QUOTE, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.SINGLE_QUOTE, char, position, line, l.filename)
   case '"':
-    return newToken(token.QUOTE, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.QUOTE, char, position, line, l.filename)
   case ',':
-    return newToken(token.COMMA, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.COMMA, char, position, line, l.filename)
   case ';':
-    return newToken(token.SEMICOLON, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.SEMICOLON, char, position, line, l.filename)
   case '-':
-    return newToken(token.MINUS, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.MINUS, char, position, line, l.filename)
   case '!':
-    firstChar := l.readChar()
+    firstChar, position, line := l.readChar()
 
     if l.peekChar() == '=' {
-      secondChar := l.readChar()
+      secondChar, _, _ := l.readChar()
       literal := string(firstChar) + string(secondChar)
-      return token.Token{Type: token.NOT_EQ, Literal: literal}
+      return newComplexToken(token.NOT_EQ, literal, position, line, l.filename)
     } else {
-      return newToken(token.BANG, firstChar)
+      return newToken(token.BANG, firstChar, position, line, l.filename)
     }
   case '*':
-    return newToken(token.ASTERISK, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.ASTERISK, char, position, line, l.filename)
   case '/':
-    return newToken(token.SLASH, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.SLASH, char, position, line, l.filename)
   case '<':
-    return newToken(token.LT, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.LT, char, position, line, l.filename)
   case '>':
-    return newToken(token.GT, l.readChar())
+    char, position, line := l.readChar()
+    return newToken(token.GT, char, position, line, l.filename)
   case 0:
-    return token.Token{Type: token.EOF, Literal: ""}
+    return newComplexToken(token.EOF, "", l.charCount, l.lineCount, l.filename)
   default:
     if isLetter(l.peekChar()) {
-      ident := l.readIdentifier()
+      ident, position, line := l.readIdentifier()
       t := token.LookupIdent(ident)
-      return token.Token{Type: t, Literal: ident}
+      return newComplexToken(t, ident, position, line, l.filename)
     } else if isDigit(l.peekChar()) {
-      return token.Token{Type: token.INT, Literal: l.readNumber()}
+      number, position, line := l.readNumber()
+      return newComplexToken(token.INT, number, position, line, l.filename)
     }else {
-      return newToken(token.ILLEGAL, l.readChar())
+      char, position, line := l.readChar()
+      return newToken(token.ILLEGAL, char, position, line, l.filename)
     }
   }
 }
 
-func newToken(tokenType token.TokenType, ch byte, pos int, line int) token.Token {
-  return token.Token{Type: tokenType, Literal: string(ch), Filename: l.filename, Line: line, Character: pos}
+func newToken(tokenType token.TokenType, ch byte, pos int, line int, fileName string) token.Token {
+  return token.Token{Type: tokenType, Literal: string(ch), Filename: fileName, Line: line, Character: pos}
+}
+
+func newComplexToken(tokenType token.TokenType, literal string, pos int, line int, fileName string) token.Token {
+  return token.Token{Type: tokenType, Literal: literal, Filename: fileName, Line: line, Character: pos}
 }
 
 func (l *Lexer) skipWhitespace() {
   var ch = l.peekChar()
 
   for ch == ' ' ||  ch == '\t' || ch == '\n' || ch == '\r' {
+    if ch == '\n' {
+      l.lineCount += 1
+      l.charCount = 0;
+    }
+
     l.readChar()
     ch = l.peekChar()
   }
 }
 
 
-func (l *Lexer) readCharGroup(fn (func(byte) bool)) string {
-  position := l.peekPosition
+func (l *Lexer) readCharGroup(fn (func(byte) bool)) (string, int, int) {
+  charPos := l.charCount
+  position:= l.peekPosition
   for fn(l.peekChar()) {
     l.readChar()
   }
 
-  return l.input[position:l.peekPosition]
+  return l.input[position:l.peekPosition], charPos, l.lineCount
 }
 
-func (l *Lexer) readIdentifier() string {
+//allow numbers in identifiers (if not first character)
+func (l *Lexer) readIdentifier() (string, int, int) {
   return l.readCharGroup(isLetter)
 }
 
-func (l *Lexer) readNumber() string {
+func (l *Lexer) readNumber() (string, int, int) {
   return l.readCharGroup(isDigit)
 }
 
--- repl/repl.go
+++ repl/repl.go
@@ -21,7 +21,7 @@ func Start(in io.Reader, out io.Writer) {
     }
 
     line := scanner.Text()
-    l := lexer.NewLexer(line)
+    l := lexer.NewLexer(line, "repl")
 
     for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
       fmt.Printf("%+v\n", tok)
